5b849358acf38a317683848d8d11f075e0b38cf4
Yuriy Volyanyuk <yzik333@outlook.com>
2018-05-31 15:06:21 +0300

    tuda je

--- ClothWPF/Arrival/AddProduct.xaml.cs
+++ ClothWPF/Arrival/AddProduct.xaml.cs
@@ -21,50 +21,20 @@ namespace ClothWPF
     /// </summary>
     public partial class AddProduct : Window
     {
-        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\iKela\Desktop\Firstdbadonet.mdf;Integrated Security=True;Connect Timeout=30");
-
         public AddProduct()
         {
             InitializeComponent();
-
-            DataContext = new MainViewModel();
+            
         }
-        //Need add on form xml (DateArrival)
+       
         private void btn_Add_Click(object sender, RoutedEventArgs e)
         {
-            //if (cmb_Name.SelectedItem != null)
-            //{
-            //    connection.Open();
-            //    string qwery = $"INSERT into Arrival(DateArrival, ManufactureDate, PriceDollar, PriceRetail, PriceWholesale, Metric)" +
-            //        $"VALUES('{DateArrival.Text}', '{txt_ManufactureDate.Text}', '{txt_SuppierPrice.Text}', '{txt_PriceRetailer.Text}','{txt_Count.Text}')";
-            //    SqlCommand command = new SqlCommand(qwery, connection);
-            //    command.ExecuteNonQuery();
-            //    connection.Close();
-            //    MessageBox.Show("Додано!");
-            //}
-            //else
-            //    MessageBox.Show("Не вибраний продукт!");
+            
         }
 
         private void cmb_Name_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string query = $"SELECT * From Product where  Name = '{cmb_Name.SelectedItem}'";
-
-            connection.Open();
-            SqlCommand command = new SqlCommand(query, connection);
             
-            SqlDataReader sqlReader = command.ExecuteReader();
-            while (sqlReader.Read())
-            {
-                txt_Count.Text = sqlReader["Metric"].ToString();
-                // txt_Discont.Text = sqlReader["Date"].ToString();
-                txt_ManufactureDate.Text = sqlReader["ManufactureDate"].ToString();
-                txt_PriceRetailer.Text = sqlReader["PriceRetail"].ToString();
-                txt_ProductCode.Text = sqlReader["KodProductu"].ToString();
-                txt_SuppierPrice.Text = sqlReader["PriceDollar"].ToString();
-            }
-            sqlReader.Close();
-            connection.Close();
         }
 
         private void cmb_Name_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
