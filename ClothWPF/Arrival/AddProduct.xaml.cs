7932ddebba5019069a25131a0b41dc32ab790910
Yuriy Volyanyuk <yzik333@outlook.com>
2018-06-18 11:22:07 +0300

    12333

--- ClothWPF/Arrival/AddProduct.xaml.cs
+++ ClothWPF/Arrival/AddProduct.xaml.cs
@@ -73,27 +73,7 @@ namespace ClothWPF
             Double.TryParse(txt_PriceRetailer.Text, out retailerPrice);
             Double.TryParse(txt_SuppierPrice.Text, out priceDollar);
             #endregion
-            //try
-            //{
-            //    //реалізувати додавання в продукт
-            //    context.ArrivalProducts.Add(new ArrivalProduct
-            //    {
-            //        Count = count,
-            //        ManufactureDate = Convert.ToDateTime(txt_ManufactureDate),
-            //        PriceDollar = priceDollar,
-            //        PriceWholesale = wholesalePrice,
-            //        PriceRetail = retailerPrice,
-            //        //Idarrival = 
-            //        Idproduct = productModels[cmb_Name.SelectedIndex].Id
-            //    });
-            //    context.SaveChanges();
-            //    MessageBox.Show("Save");
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
-            
+           
             _name = cmb_Name.Text;
             _code = txt_ProductCode.Text;
             _count = count;
