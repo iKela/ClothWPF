40a5b302e5c29c75681c456a2350b4181163b10c
Kovalchuk Nazar <kovalchuknm1997@gmail.com>
2018-06-20 15:06:30 +0300

    sdfgsdf

--- ClothWPF/AddItem.xaml.cs
+++ ClothWPF/AddItem.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace ClothWPF
 {
@@ -24,55 +25,63 @@ namespace ClothWPF
         
         private void btn_Add_Click(object sender, RoutedEventArgs e)
         {
-            #region Double Parse
-            double wholesalePrice = 0;
-            double retailerPrice = 0;
-            double priceDollar = 0;
-            double priceUah = 0;
-            Double.TryParse(txt_PriceWholesale.Text, out wholesalePrice);
-            Double.TryParse(txt_PriceRetail.Text, out retailerPrice);
-            Double.TryParse(txt_PriceDollar.Text, out priceDollar);
-            Double.TryParse(txt_PriceUah.Text, out priceUah);
-            #endregion            
-            using (EfContext context = new EfContext())
+            if (!String.IsNullOrEmpty(txt_Name.Text))
             {
-                try
+                #region Double Parse
+                double wholesalePrice = 0;
+                double retailerPrice = 0;
+                double priceDollar = 0;
+                double priceUah = 0;
+                Double.TryParse(txt_PriceWholesale.Text, out wholesalePrice);
+                Double.TryParse(txt_PriceRetail.Text, out retailerPrice);
+                Double.TryParse(txt_PriceDollar.Text, out priceDollar);
+                Double.TryParse(txt_PriceUah.Text, out priceUah);
+                #endregion
+                using (EfContext context = new EfContext())
                 {
-                    if (Productadding != null)
+                    try
                     {
-                        var product = context.Products.Where(c => c.IdProduct == Productadding.IdProduct).FirstOrDefault();
-                        product.Name = txt_Name.Text;
-                        product.Article = txt_Article.Text;
-                        product.Code = txt_ProductCode.Text;
-                        product.PriceDollar = priceDollar;
-                        product.PriceUah = priceUah;
-                        product.PriceRetail = retailerPrice;
-                        product.PriceWholesale = wholesalePrice;
-                        product.Country = cmb_Country.Text;
-                        Productadding = product;
-                        context.SaveChanges();
-                    }
-                    else
-                    {
-                        context.Products.Add(new Product
+                        if (Productadding != null)
                         {
-                            Name = txt_Name.Text,
-                            Article = txt_Article.Text,
-                            Code = txt_ProductCode.Text,
-                            PriceDollar = priceDollar,
-                            PriceUah = priceUah,
-                            PriceRetail = retailerPrice,
-                            PriceWholesale = wholesalePrice,
-                            Country = cmb_Country.Text
-                        });
-                        context.SaveChanges();
+                            var product = context.Products.Where(c => c.IdProduct == Productadding.IdProduct).FirstOrDefault();
+                            product.Name = txt_Name.Text;
+                            product.Article = txt_Article.Text;
+                            product.Code = txt_ProductCode.Text;
+                            product.PriceDollar = priceDollar;
+                            product.PriceUah = priceUah;
+                            product.PriceRetail = retailerPrice;
+                            product.PriceWholesale = wholesalePrice;
+                            product.Country = cmb_Country.Text;
+                            Productadding = product;
+                            context.SaveChanges();
+                        }
+                        else
+                        {
+                            context.Products.Add(new Product
+                            {
+                                Name = txt_Name.Text,
+                                Article = txt_Article.Text,
+                                Code = txt_ProductCode.Text,
+                                PriceDollar = priceDollar,
+                                PriceUah = priceUah,
+                                PriceRetail = retailerPrice,
+                                PriceWholesale = wholesalePrice,
+                                Country = cmb_Country.Text
+                            });
+                            context.SaveChanges();
+                        }
+                        MessageBox.Show("Зберкженно!!!");
                     }
-                    MessageBox.Show("Зберкженно!!!");
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
+
+            }
+            else
+            {
+                MessageBox.Show("Обов`язкові поля мають бути заповненні!", "Увага!", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
         
