42b6fc83f3a0d42ba9c4ab6f4700ae348ab504dd
Kovalchuk Nazar <kovalchuknm1997@gmail.com>
2018-08-13 17:58:31 +0300

    Realization Window

---
+++ ClothWPF/General/Realization/RealizationWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClothWPF.General.Realization
+{
+    /// <summary>
+    /// Interaction logic for RealizationWindow.xaml
+    /// </summary>
+    public partial class RealizationWindow : Window
+    {
+        public RealizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Calculation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
