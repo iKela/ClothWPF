12fa5ae6647e5187eb9e8df0b8f74aa02a0b6518
Kovalchuk Nazar <kovalchuknm1997@gmail.com>
2018-09-11 17:40:50 +0300

    sdg

--- ClothWPF/Arrival/AddProduct.xaml.cs
+++ ClothWPF/Arrival/AddProduct.xaml.cs
@@ -149,6 +149,7 @@ namespace ClothWPF
                 box.Foreground = new SolidColorBrush(Colors.Black);
                 hasBeenClicked = true;
             }
+            hasBeenClicked = false;
         }
     }
 }
