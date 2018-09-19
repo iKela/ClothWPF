c67682a989a8494cf9d5c6a2d64fa78713bee1db
Yuriy Volyanyuk <yzik333@outlook.com>
2018-09-19 17:56:09 +0300

    helloWorld

--- ClothWPF/Entities/Enterprise.cs
+++ ClothWPF/Entities/Enterprise.cs
@@ -11,18 +11,18 @@ namespace ClothWPF.Entities
     {
         [Key]
         public int IdEnterprise { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-        public string Adress { get; set; }
-        public string KodEDRPOU { get; set; }
-        public string Ownership { get; set; } //За формою властності
-        public string CreatingWay { get; set; } //Залужно від способу утворення та формування статутного капіталу
-        public string LegalForm { get; set; }//Залежно від організаційно-правової форми 
-        public string Activity { get; set; }//Вид діяльності 
-        public string IdentificationCode { get; set; }
-        public string Director { get; set; }
-        public string Email { get; set; }
-        public string Fax { get; set; }
-        public string Number { get; set; }
+        public string Name                                        { get; set; }
+        public string City                                       { get; set; }
+        public string Adress                                    { get; set; }
+        public string KodEDRPOU                      { get; set; }
+        public string Ownership                  { get; set; } //За формою властності
+        public string CreatingWay                     { get; set; } //Залужно від способу утворення та формування статутного капіталу
+        public string LegalForm                     { get; set; }//Залежно від організаційно-правової форми 
+        public string Activity                         { get; set; }//Вид діяльності 
+        public string IdentificationCode             { get; set; }
+        public string Director                         { get; set; }
+        public string Email                            { get; set; }
+        public string Fax                           { get; set; }
+        public string Number                      { get; set; }
     }
 }
